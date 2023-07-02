e370995a59342353e405666a5147b21521cee573
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-07-02 22:14:00 +0400

    m5Start

--- Module04/ex01/main.cpp
+++ Module04/ex01/main.cpp
@@ -6,33 +6,35 @@
 
 int main()
 {
-	int n = 4;
-	Animal	*animals[n];
-	for (int i = 0; i < n / 2; i++)
-		animals[i] = new Dog();
-	for (int i = n /2 ; i < n; i++)
-		animals[i] = new Cat();
-	for (int i = 0; i < n; i++)
-		animals[i]->makeSound();
-	for (int i = 0; i < n; i++)
-		delete animals[i];
-	std::cout<<"######################################################################\n";
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete j;//should not create a leak
-	delete i;
-	std::cout<<"######################################################################\n";
-	WrongAnimal *wa = new WrongCat();
-	wa->makeSound();
-	delete wa;
-	std::cout<<"######################################################################\n";
-	// Animal *k;
+	// int n = 4;
+	// Animal	*animals[n];
+	// for (int i = 0; i < n / 2; i++)
+	// 	animals[i] = new Dog();
+	// for (int i = n /2 ; i < n; i++)
+	// 	animals[i] = new Cat();
+	// for (int i = 0; i < n; i++)
+	// 	animals[i]->makeSound();
+	// for (int i = 0; i < n; i++)
+	// 	delete animals[i];
+	// std::cout<<"######################################################################\n";
+	// const Animal* j = new Dog();
+	// const Animal* i = new Cat();
+	// delete j;//should not create a leak
+	// delete i;
+	// std::cout<<"######################################################################\n";
+	// WrongAnimal *wa = new WrongCat();
+	// wa->makeSound();
+	// delete wa;
+	// std::cout<<"######################################################################\n";
+	// // Animal *k;
 	Cat	m;
 	m.setIdea(7, "I have fun\n");
 	// k = &m;
 	// k->makeSound();
 	Cat	s;
 	s = m;
+	std::cout<<m.getIdea(7);
+	std::cout<<s.getIdea(7);
 	m.setIdea(7, "now coool\n");
 	std::cout<<m.getIdea(7);
 	std::cout<<s.getIdea(7);
